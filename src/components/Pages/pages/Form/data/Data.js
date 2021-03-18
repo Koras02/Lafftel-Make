0ee07fc0255a6e74a442562edfc79e741212503d
Koras02 <wjdgh0727@gmail.com>
2021-03-19 04:00:14 +0900

    요일별 수정

--- src/components/Pages/pages/Form/data/Data.js
+++ src/components/Pages/pages/Form/data/Data.js
@@ -1,6 +1,18 @@
 import Image from "./Images/Ataack of Titan.jpg";
+import Image2 from "./Images/Bull.jpg";
+import Image3 from "./Images/NonNon.jpg";
+import Image4 from "./Images/BanYo.jpg";
+import Image5 from "./Images/Basket.jpg";
 
 export const HomeData1 = {
-  name: "시발",
+  name: "진격의 거인 The FINAL",
+  name2: "불꽃 소방대",
+  name3: "논논비요리:논스톱",
+  name4: "반요 야사히메",
+  name5: "후르츠 바스켓:더 파이널 시즌",
   img: Image,
+  img2: Image2,
+  img3: Image3,
+  img4: Image4,
+  img5: Image5,
 };
