d5cbcf77689b54cb1265aea37f1a633ee006d6b3
Koras02 <wjdgh0727@gmail.com>
2021-03-19 03:26:51 +0900

    요일별 신작 추가

--- src/components/MainVisual.js
+++ src/components/MainVisual.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import Header from './Header';
+import React from "react";
+import Header from "./Header";
 
-import 'slick-carousel/slick/slick.css';
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
- 
-import MainPage from './Pages/pages/MainVisual';
- 
+
+import MainPage from "./Pages/pages/MainVisual";
+import NewAnime from "./Pages/pages/NewAnime";
 
 export function MainVisual() {
-     return (
-         <>
-         <Header />
-         <MainPage />
-         </>
-     );
-   }
+  return (
+    <>
+      <Header />
+      <MainPage />
+      <NewAnime />
+    </>
+  );
+}
 
-
-   export default MainVisual
+export default MainVisual;
--- src/components/Pages/pages/style/MainVisualStyle.js
+++ src/components/Pages/pages/style/MainVisualStyle.js
@@ -1,10 +1,30 @@
-import styled from 'styled-components';
-
+import styled from "styled-components";
 
 export const List = styled.div`
-  width:100%;
-  height:20px;
-//   border:1px solid red;  
-  position:absolute;
-  left:20%;
-`
+  width: 100%;
+  height: 20px;
+  //   border:1px solid red;
+  position: absolute;
+  left: 20%;
+`;
+
+export const Click = styled.div`
+  display: inline-flex;
+  margin: 15px;
+  width: 40px;
+  height: 40px;
+  // border:1px solid #000;
+  border-radius: 100%;
+  padding: 10px;
+  text-align: center;
+  background: ${({ click }) => (click ? "red" : "blue")};
+`;
+
+export const List1 = styled.div`
+  text-align: center;
+  position: relative;
+  left: 10px;
+  top: 5px;
+  font: bold 18px/24px arial;
+  color: #fff;
+`;
